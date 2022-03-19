9e94e276840a003eac0fb02d53075242069ab489
Eike Martin Löhden <eike@loehden.eu>
2022-03-19 16:07:14 +0100

    Adding new functionalities to load_tweets.py

--- load_tweets.py
+++ load_tweets.py
@@ -1,15 +1,15 @@
-import tweepy
 import pandas as pd
 import requests
 import json
 import datetime
 import pytz
 
-def connect_api(ini_file: str) -> str:
+def header_authorization(ini_file: str) -> dict:
     """
-    Function returns the bearer-token of the access informations in the ini_file.
+    Function returns the header including the bearer-token of the access
+    informations in the ini_file to connect with the api.
 
-    :param ini_file: the path to the ini-file.
+    :param ini_file: The path to the ini-file.
     :return: The bearer token
     """
     access_info = {}
@@ -17,26 +17,32 @@ def connect_api(ini_file: str) -> str:
         for l in f.readlines():
             tmp = l.split(': ')
             access_info[tmp[0]] = tmp[1].strip('\n')
-    #client = tweepy.Client(consumer_key = access_info['api_key'], consumer_secret = access_info['api_secret'],
-    #                        access_token = access_info['access_token'], access_token_secret = access_info['access_secret'])
+
+    return {"Authorization": "Bearer {}".format(access_info["bearer_token"])}
 
 
-    return access_info["bearer_token"]
+def create_url_params(query: str, start_date: str=None, end_date: str=None,
+                        max_results:int = 100) -> tuple:
+    """
+    Creating URL and parameters for the connection to the Twitter_API.
 
-
-def create_headers(bearer_token):
-    headers = {"Authorization": "Bearer {}".format(bearer_token)}
-    return headers
-
-def create_url(keyword, start_date=None, end_date=None, max_results = 100):
+    :param query: The query containing the keywords for the search.
+    :param start_date: The startdate of the search in ISO-Format: YYYY:MM:DDTHH:MM:SSZ
+    :param end_date: The enddate of the search in ISO-Format.
+    :param max_results: The maximum results of the search.
+    :return: Returns the URL and the parameters as a tuple (url, parameters)
+    """
     if start_date is None and end_date is None:
-        end_date = datetime.datetime.now(pytz.utc) - datetime.timedelta(seconds=10)
-        start_date = str(end_date - datetime.timedelta(days=6, hours=23, minutes=59)).replace(' ', 'T')[:-7] + 'Z'
+        # Creating the dateinformation based on the current time minus one week.
+        end_date = datetime.datetime.now(pytz.utc)-datetime.timedelta(seconds=10)
+        start_date = str(end_date - datetime.timedelta(days=6,
+                                                        hours=23,
+                                                        minutes=59)
+                        ).replace(' ', 'T')[:-7] + 'Z'
         end_date = str(end_date).replace(' ', 'T')[:-7] + 'Z'
-    search_url = "https://api.twitter.com/2/tweets/search/recent" #Change to the endpoint you want to collect data from
-
+    url = "https://api.twitter.com/2/tweets/search/recent"
     #change params based on the endpoint you are using
-    query_params = {'query': keyword,
+    query_params = {'query': query,
                     'start_time': start_date,
                     'end_time': end_date,
                     'max_results': max_results,
@@ -45,16 +51,37 @@ def create_url(keyword, start_date=None, end_date=None, max_results = 100):
                     'user.fields': 'id,name,username,created_at,description,entities,public_metrics,verified',
                     'place.fields': 'full_name,country,geo',
                     'next_token': {}}
-    return (search_url, query_params)
+    return (url, query_params)
 
-def connect_to_endpoint(url, headers, params, next_token = None):
-    params['next_token'] = next_token   #params object received from create_url function
-    response = requests.request("GET", url, headers = headers, params = params)
+def connect_to_api(url: str, headers: dict, params: dict, next_token: str = None):
+    """
+    The connection to the Twitter API based on the given parameters.
+
+    :param url: The url of the Twitter-API endpoint.
+    :param headers: The header information for the connection containing the bearer-token.
+    :param params: The parameters for the search as a dictonary.
+    :param next_token: The next_token only given if there are unseen results from the last search.
+    :return: Returns the response in JSON format.
+    """
+    params['next_token'] = next_token
+    try:
+        response = requests.request("GET", url, headers = headers, params = params)
+    except requests.exceptions.ConnectionError:
+        print("No internet connection possible!")
+        return None
     print("Endpoint Response Code: " + str(response.status_code))
     if response.status_code != 200:
         raise Exception(response.status_code, response.text)
     return response.json()
 
-def search_tweets(query: str, next_token = None):
-    url, params = create_url(query)
-    return connect_to_endpoint(url, create_headers(connect_api('twitter_api.ini')), params, next_token)
+def search_tweets(query: str, next_token:str = None, max_results: int=100):
+    """
+    Search tweets by connecting to the Twitter-API while using the existing
+    functions.
+
+    :param query: The query containing the keywords for the search.
+    :param next_token: The next_token only given if there are unseen results from the last search.
+    :return: The API-response to the search as JSON-format.
+    """
+    url, params = create_url_params(query, max_results=max_results)
+    return connect_to_api(url, header_authorization('twitter_api.ini'), params, next_token)
